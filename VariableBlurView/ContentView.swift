1d090e88ab72184a41e078ef3e96d2aa2c76a5f5
Janum Trivedi <jtrivedi@me.com>
2023-05-29 08:10:03 -0700

    Initial Commit

---
+++ VariableBlurView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  VariableBlurView
+//
+//  Created by Janum Trivedi on 5/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VariableBlurView/VariableBlurViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariableBlurViewApp.swift
+//  VariableBlurView
+//
+//  Created by Janum Trivedi on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct VariableBlurViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
